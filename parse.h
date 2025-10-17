f8f17ddef97220ef834ff0e125b245adfef9576e
dolandeer <dolandeering@gmail.com>
2025-10-17 00:22:32 -0400

    Preparing parse.h to be written.

--- parse.h
+++ parse.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
 //JSON Parsing
 
 
 
 
-//CSV Parsing
+//NOAA Parsing (treat ,, as end of row and ignore narratives)
 
+
+
+
+//USCities Parsing (use a for loop to ignore any unwanted data)
