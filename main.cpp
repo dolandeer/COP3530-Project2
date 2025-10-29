eed757dbe57148f32c5db53eaedc7f08d4c0f4ec
dolandeer <dolandeering@gmail.com>
2025-10-29 12:45:52 -0400

    Updated example weather API usage to correctly show an example API call.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <cpr/cpr.h>
 #include "crow_all.h"
-#include "parse.h"
+//#include "parse.h"
 
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     // example API call. max of 10,000 calls a day for open-meteo so call as needed do not loop!
-    /*
-    cpr::Response r = cpr::Get(cpr::Url{"https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&forecast_days=1"});
+
+    cpr::Response r = cpr::Get(cpr::Url{"https://api.open-meteo.com/v1/forecast?latitude=29.6516&longitude=-82.3248&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,weather_code&timezone=America%2FNew_York&past_days=5&forecast_days=1&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch"});
     std::cout << r.status_code << std::endl;                  // 200
     std::cout << r.header["content-type"] << std::endl;       // application/json; charset=utf-8
     std::cout << r.text;                         // JSON text string
-    */
+
 
     //TEST
 
